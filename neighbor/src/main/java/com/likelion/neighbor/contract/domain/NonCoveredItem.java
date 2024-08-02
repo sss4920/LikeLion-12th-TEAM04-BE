6877b352c8c70b8fd774a4d6af6e28a5ebe91bc5
soohyun <sss4920@naver.com>
2024-08-02 22:45:05 +0900

    #11 feature: 비급여항목 엔티티 추가

---
+++ neighbor/src/main/java/com/likelion/neighbor/contract/domain/NonCoveredItem.java
@@ -0,0 +1,25 @@
+package com.likelion.neighbor.contract.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class NonCoveredItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String middleName;
+
+	private String shortName;
+
+}
