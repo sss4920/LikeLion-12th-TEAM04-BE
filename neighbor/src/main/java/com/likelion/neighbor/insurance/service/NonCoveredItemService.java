dfe02d74914333faf2296d509953334403cf74b8
soohyun <sss4920@naver.com>
2024-08-06 16:34:59 +0900

    #30 refactor: 비급여 검색 유저 토큰검사 추가

--- neighbor/src/main/java/com/likelion/neighbor/insurance/service/NonCoveredItemService.java
+++ neighbor/src/main/java/com/likelion/neighbor/insurance/service/NonCoveredItemService.java
@@ -1,7 +1,12 @@
 package com.likelion.neighbor.insurance.service;
 
 import com.likelion.neighbor.contract.domain.NonCoveredItem;
+import com.likelion.neighbor.global.exception.NotFoundException;
+import com.likelion.neighbor.global.exception.model.Error;
 import com.likelion.neighbor.insurance.domain.repository.NonCoveredItemRepository;
+import com.likelion.neighbor.user.domain.User;
+import com.likelion.neighbor.user.domain.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,26 +14,39 @@ import org.springframework.stereotype.Service;
 public class NonCoveredItemService {
 
     private final NonCoveredItemRepository nonCoveredItemRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public NonCoveredItemService(NonCoveredItemRepository nonCoveredItemRepository) {
+    public NonCoveredItemService(NonCoveredItemRepository nonCoveredItemRepository, UserRepository userRepository) {
         this.nonCoveredItemRepository = nonCoveredItemRepository;
+        this.userRepository = userRepository;
+
     }
 
-    public boolean isNonCovered(String middleName, String shortName) {
+    public boolean isNonCovered(String middleName, String shortName, String userId) {
+        hasUserPermission(userId);
         return nonCoveredItemRepository.existsByMiddleNameAndShortName(middleName, shortName);
     }
 
-    public boolean existsByMiddleName(String middleName) {
+    public boolean existsByMiddleName(String middleName, String userId) {
+        hasUserPermission(userId);
         return nonCoveredItemRepository.existsByMiddleName(middleName);
     }
 
-    public boolean existsByShortName(String shortName) {
+    public boolean existsByShortName(String shortName, String userId) {
+        hasUserPermission(userId);
         return nonCoveredItemRepository.existsByShortName(shortName);
     }
 
-    public NonCoveredItem addNonCoveredItem(String middleName, String shortName) {
+    public NonCoveredItem addNonCoveredItem(String middleName, String shortName, String userId) {
+        hasUserPermission(userId);
         NonCoveredItem newItem = new NonCoveredItem(null, middleName, shortName);
         return nonCoveredItemRepository.save(newItem);
+    }
+
+    private User hasUserPermission(String userId){
+        return userRepository.findById(Long.parseLong(userId)).orElseThrow(
+            () -> new NotFoundException(Error.MEMBERS_NOT_FOUND_EXCEPTION, Error.MEMBERS_NOT_FOUND_EXCEPTION.getMessage())
+        );
     }
 }
