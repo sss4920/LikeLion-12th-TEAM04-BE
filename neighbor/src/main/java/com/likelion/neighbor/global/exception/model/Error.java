54c54738962506bf9fe9e05ed4337c3795f94cee
soohyun <sss4920@naver.com>
2024-08-06 20:28:54 +0900

    #32 refactor: 예외케이스 추가

--- neighbor/src/main/java/com/likelion/neighbor/global/exception/model/Error.java
+++ neighbor/src/main/java/com/likelion/neighbor/global/exception/model/Error.java
@@ -15,6 +15,7 @@ public enum Error {
 	 */
 	MEMBERS_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 유저가 없습니다."),
 	INSURANCE_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 보험을 찾을 수 없습니다."), // 추가된 부분
+	LOGIN_PARAMETER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "로그인 취소"),
 
 	/**
 	 * 400 BAD REQUEST
@@ -23,6 +24,19 @@ public enum Error {
 	PARAMETER_NOT_VALID_ERROR(HttpStatus.BAD_REQUEST, "파라미터가 적절치 않습니다."),
 	EXTENSION_NOT_VALID_ERROR(HttpStatus.BAD_REQUEST, "지원하지 않는 확장자입니다."),
 	EXIST_USER_ERROR(HttpStatus.BAD_REQUEST, "이미 네이보에 회원가입이 되어있는 유저입니다. 로그인을 해주세요."),
+	ID_BAD_REQUEST(HttpStatus.BAD_REQUEST, "아이디 자릿수가 맞지않습니다."),
+	ID_FORM_BAD_REQUEST(HttpStatus.BAD_REQUEST, "아이디 형식이 맞지 않습니다."),
+	ID_REGISTERED_BAD_REQUEST(HttpStatus.BAD_REQUEST, "이미 등록된 아이디입니다. 다른 아이디로 해주세요"),
+	PASSWORD_BAD_REQUEST(HttpStatus.BAD_REQUEST, "비밀번호 자릿수 오류입니다."),
+	PASSWORD_FORM_BAD_REQUEST(HttpStatus.BAD_REQUEST, "비밀번호 형식 오류"),
+	EMAIL_BAD_REQUEST(HttpStatus.BAD_REQUEST, "이메일 이미 등록되었습니다. 다른 이메일 주소를 입력해주세요."),
+	EMAIL_FORM_BAD_REQUEST(HttpStatus.BAD_REQUEST, "이메일 형식 오류입니다."),
+	EMAIL_IS_NOT_VALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 이메일입니다."),
+
+
+
+
+
 
 	/**
 	 * 401 UNAUTHORIZED
